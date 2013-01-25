82dc586258bc57e1f2ff4002374c9b5ec58418e6
Jonathan Molina <jm@nerdla.bz>
2013-01-25 08:27:11 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imapo::Application.config.secret_token = '6e4d38e956edd085e13ee0944fa676eb90767104f25686dcfb3efe1246aa68bf8d817ef7fc89cec4bc6ce4b8033ad4c8043d6a6d62c11c1b4710b7212a136595'
