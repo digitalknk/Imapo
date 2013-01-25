e4c5be643003b2eafe6b749198169453b8f26019
Jonathan Molina <jm@nerdla.bz>
2013-01-25 09:56:32 -0800

    Updated the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Imapo::Application.routes.draw do
+  get "logout" => "sessions#destroy", :as => "logout"
+  get "login" => "sessions#new", :as => "login"
+  resources :users
+  resources :sessions
+  
   root :to => "home#index"
 end
