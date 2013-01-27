7f6e8ff8f04bbe02ab8a167a8987608819ce3117
Jonathan Molina <jm@nerdla.bz>
2013-01-27 00:42:24 -0800

    Added the Images Controller, Model, Views, and the uploader.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Imapo::Application.routes.draw do
+  resources :images
+
+
   get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#new", :as => "login"
   resources :users
